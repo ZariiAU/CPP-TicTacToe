73b32d03f75bd4505c56d5dab3c5662786a49c56
Zarii <s210504@students.aie.edu.au>
2021-06-21 14:57:39 +0930

    Comments!!

--- PlayerProfileDatabase/PlayerBoard.cpp
+++ PlayerProfileDatabase/PlayerBoard.cpp
@@ -17,6 +17,7 @@ PlayerBoard::~PlayerBoard()
 	delete[] playerList;
 }
 
+// Draw function for the leaderboard
 void PlayerBoard::Draw(bool drawTitleCard, bool clearScreenBeforeDraw)
 {
 	if(clearScreenBeforeDraw)
@@ -28,12 +29,14 @@ void PlayerBoard::Draw(bool drawTitleCard, bool clearScreenBeforeDraw)
 		cout << "###################\n" << endl;
 	}
 
+	// Loop through players in use and draw their info for the screen
 	if (!GetPlayersInUse() == 0) {
 		for (unsigned int i = 0; i < m_used; i++) {
 			cout << i+1 << "] ";
 			playerList[i].Draw();
 		}
 	}
+	// If there are no players, do this.
 	else {
 		cout << " \n No Players Found." << endl;
 	}
@@ -55,10 +58,11 @@ void PlayerBoard::SetPlayersInUse(unsigned int numberOfPlayers)
 	m_used = numberOfPlayers;
 }
 
+// Create a player
 void PlayerBoard::AddPlayer()
-{
-	char playerNameTemp[50];
-	unsigned int playerScoreTemp = 0;
+{ 
+	char playerNameTemp[50]; // Set a temporary variable to hold the data until Player class is created
+	unsigned int playerScoreTemp = 0; // ..
 
 	system("cls");
 
@@ -68,15 +72,17 @@ void PlayerBoard::AddPlayer()
 
 	cout << "\nEnter Player Name:" << endl;
 
-	cin >> (char*)playerNameTemp; // Ask for name input and set a temp variable to hold it until Player class is created
+	cin >> (char*)playerNameTemp; // Ask for name input 
 
 	system("cls");
 
 	cout << "\nEnter Player Score:" << endl;
 
-	cin >> playerScoreTemp;
+	cin >> playerScoreTemp; // Ask for score input
 
+	// Check if stream is good.
 	while(!cin.good()){
+		// Clear the console-in buffer
 		cin.clear();
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		
@@ -85,7 +91,9 @@ void PlayerBoard::AddPlayer()
 		cin >> playerScoreTemp;
 	}
 
+	// Add a new player to the array with values from above
 	playerList[GetPlayersInUse()] = Player(playerNameTemp, playerScoreTemp);
 
+	// Increment Players in Use by 1
 	SetPlayersInUse(GetPlayersInUse() + 1);
 }
